a7cb5f9151618d9ca1fb1ede2e7e60e0baadd3d6
Ying Huang <huangyingw@gmail.com>
2024-10-22 23:02:09 -0700

    n

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,7 @@ vim.o.termguicolors = false
 -- 设置当前的颜色方案为 'vim'
 vim.cmd('colorscheme vim')
 
+vim.cmd 'source ~/.vim/plugin/common.vim'
 vim.cmd 'source ~/.vimrc'
 
 require('plugins')
@@ -15,24 +16,93 @@ require('keymaps')
 require('nvim_molten_config')
 -- require('secret_config')
 
---[[
-require("chatgpt").setup()
+-- 引导 lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-local chatgpt = require("chatgpt")
-local wk = require("which-key")
-
-wk.register({
-    p = {
-        name = "ChatGPT",
-        e = {
-            function()
-                chatgpt.edit_with_instructions()
-            end,
-            "Edit with instructions",
-        },
+-- 设置插件
+require("lazy").setup({
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      -- 在这里添加任何选项
     },
-}, {
-    prefix = "<leader>",
-    mode = "v",
+    build = "make BUILD_FROM_SOURCE=true",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 })
-]]
+
+-- 在文件顶部添加
+local function debug_print(msg)
+  print(msg)
+  vim.cmd('messages')
+end
+
+-- 在加载 avante_lib 之前添加
+debug_print("Attempting to load avante_lib")
+local ok, err = pcall(require, 'avante_lib')
+if not ok then
+  debug_print("Failed to load avante_lib: " .. tostring(err))
+else
+  debug_print("Successfully loaded avante_lib")
+end
+
+-- 加载 avante_lib
+require('avante_lib').load()
+
+-- 在设置 avante 之前添加
+debug_print("Attempting to setup avante")
+ok, err = pcall(require('avante').setup, {
+  -- 您的配置在这里
+})
+if not ok then
+  debug_print("Failed to setup avante: " .. tostring(err))
+else
+  debug_print("Successfully setup avante")
+end
+
+-- 设置推荐的 Neovim 选项
+vim.opt.laststatus = 3
+
+package.cpath = package.cpath .. ";" .. vim.fn.stdpath("data") .. "/lazy/avante.nvim/lua/avante/?.dylib"
